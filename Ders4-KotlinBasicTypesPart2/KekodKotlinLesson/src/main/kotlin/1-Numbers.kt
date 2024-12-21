da0a34eb6c1d5b57cb52287c697ceda85cc9249b
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2024-12-21 10:41:32 +0300

    feat/docs: Added examples and explanations for numeric types and type inference

--- Ders4-KotlinBasicTypesPart2/KekodKotlinLesson/src/main/kotlin/1-Numbers.kt
+++ Ders4-KotlinBasicTypesPart2/KekodKotlinLesson/src/main/kotlin/1-Numbers.kt
@@ -24,8 +24,76 @@ fun main() {
     * */
 
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     //Değişkene değer ataması yapılırken maksimum performans almak için doğru değişken tipini kullanmak önemlidir.
     //Bunun için değişkenlerin değer aralıklarını yukarıdan kontrol edebilirsiniz.
     //Tip çıkarımı (Type Inference) sırasında eğer sayı Int ifadeden büyük değilse tipi default olarak Int set edilir
-    //Tip çıkarımı (Type Inference) sırasında eğer sayı Int ifadeden büyükse tipi default
+    //Tip çıkarımı (Type Inference) sırasında eğer sayı Int ifadeden büyükse tipi default Long set edilir.
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    val minByteValue: Byte = Byte.MIN_VALUE
+    val maxByteValue: Byte = Byte.MAX_VALUE
+
+    val minShortValue: Short = Short.MIN_VALUE
+    val maxShortValue: Short = Short.MAX_VALUE
+
+    val minIntValue: Int = Int.MIN_VALUE
+    val maxIntValue: Int = Int.MAX_VALUE
+
+    val minLongValue: Long = Long.MIN_VALUE
+    val maxLongValue: Long = Long.MAX_VALUE
+
+    val minFloatValue: Float = Float.MIN_VALUE
+    val maxFloatValue: Float = Float.MAX_VALUE
+
+    val minDoubleValue: Double = Double.MIN_VALUE
+    val maxDoubleValue: Double = Double.MAX_VALUE
+
+    println("minByteValue : " + minByteValue)
+    println("maxByteValue : " + maxByteValue)
+    println("minShortValue : " + minShortValue)
+    println("maxShortValue : " + maxShortValue)
+    println("minIntValue : " + minIntValue)
+    println("maxIntValue : " + maxIntValue)
+    println("minLongValue : " + minLongValue)
+    println("maxLongValue : " + maxLongValue)
+    println("minFloatValue : " + minFloatValue)
+    println("maxFloatValue : " + maxFloatValue)
+    println("minDoubleValue : " + minDoubleValue)
+    println("maxDoubleValue : " + maxDoubleValue)
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    // Long         değişkenler için sayının sonuna "L" konularak değer ataması yapılabilir.
+    // Float        değişkenler için sayının sonuna "F" ve "f" konularak değer ataması yapılabilir.
+    // Double       değişkenler için geleneksel gösterim desteklenir.
+    // Decimal      tanımı yapılabilir       [0-9]
+    // Octal        tanımı yapılamıyor       [0-7]
+    // Hexadecimal  tanımı yapılabilir
+    // Binary       tanımı yapılabilir       [0-1]
+
+    val longNumber = 1586L
+    val floatNumber1 = 19.90F
+    val floatNumber2 = 13f
+    val doubleNumber = 3.14
+    val doubleNumber2 = 3.14e10
+    val decimalNumber = 1907
+//    val octalNumber = 0197
+    val hexadecimalNumber = 0x759
+    val binaryNumber = 0b01000011
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //Number değişken tanımı yapılırken underscore "_" kullanılabilir.
+
+    val oneMillion = 1_000_000 //1000000
+    val creditNumber = 1234_5678_9012_3456L
+    val bytes = 0b01000011_01101111_01100100_01100101_01101101_01111001
+
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 }
+//region Mülakat Soruları
+// .Type Inference nedir?
+//Eşitliğin karşı tarafında bir value varsa siz o değişkenin değişken tipini vermeseniz bile eşitliğin diğer tarafındaki değere bakarak değişkenin tipinin ne olacağına IDE karar verebilir. Bir değişken tipinin eşitliğin sağ tarafına bakarak belirlenmesi işlemine type inference(tip çıkarma) denir. Ama sayısal değerlerde verilen değer eğer Int değer aralığında ise Int set edilir Int değer aralığından daha büyükse Long set edilir.
+//endregion
