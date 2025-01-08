099c3c5ad3ca14747c0c1ab15fba3dd96736e808
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-01-08 22:06:03 +0300

    feat: Explained noinline and add Android pseudo-code example

--- 12-Ders11KotlinFunctionsInlineNoInlineCrossInlineClasses/KekodKotlinLesson/src/functions/InlineNoInlineCrossInline.kt
+++ 12-Ders11KotlinFunctionsInlineNoInlineCrossInlineClasses/KekodKotlinLesson/src/functions/InlineNoInlineCrossInline.kt
@@ -1,5 +1,6 @@
 package functions
 
+
 fun main() {
     runAndPrint({ println(it) }, { println(it) })
 }
@@ -20,3 +21,36 @@ fun log(logger: (String) -> Unit): Unit {
 
 //Higher order function'ın Parametresinde birden fazla eğer fonksiyonunuz varsa noinline kullanmak isteyebilirsiniz. Bu sayede hiçbirinin nesnesinin üretilmemesini sağlarsınız.
 
+class HomeFragment() {
+    val homeListAdapter = HomeListAdapter({
+
+    })
+}
+
+class HomeListAdapter(onClick: (View) -> Unit) {
+    init {
+        val homeListViewHolder = HomeListViewHolder()
+        homeListViewHolder.bind(onClick)
+        homeListViewHolder bind onClick
+    }
+}
+
+class HomeListViewHolder() {
+    inline infix fun bind(noinline onClick: (View) -> Unit): Unit {
+        val button = Button()
+        button.setOnClickListener(onClick)
+//        button.setOnClickListener {
+//            onClick.invoke(it)
+//        }
+    }
+}
+
+class Button() {
+    fun setOnClickListener(onClick: (View) -> Unit): Unit {
+
+    }
+}
+
+class View() {
+
+}
