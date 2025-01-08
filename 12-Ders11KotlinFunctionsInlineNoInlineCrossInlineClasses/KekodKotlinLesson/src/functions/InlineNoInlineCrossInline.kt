5cdf9dfa94af86f05836c7e54262c242c4a0f515
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-01-08 21:54:35 +0300

    feat: Explained the use of noinline in Kotlin

--- 12-Ders11KotlinFunctionsInlineNoInlineCrossInlineClasses/KekodKotlinLesson/src/functions/InlineNoInlineCrossInline.kt
+++ 12-Ders11KotlinFunctionsInlineNoInlineCrossInlineClasses/KekodKotlinLesson/src/functions/InlineNoInlineCrossInline.kt
@@ -1,14 +1,22 @@
 package functions
 
 fun main() {
-
-    repeat(10) {
-        runAndPrint {
-            println(it)
-        }
-    }
+    runAndPrint({ println(it) }, { println(it) })
 }
 
-inline fun runAndPrint(run: (String) -> Unit) {
+inline fun runAndPrint(run: (String) -> Unit, noinline logger: (String) -> Unit) {
+    log(logger)
     run("Message")
+    logger("End")
 }
+
+fun log(logger: (String) -> Unit): Unit {
+    logger("Start")
+}
+
+//inline keywordü her seferinde fonksiyondan instance oluşmasını engeller. higher order functionın içindeki datayı metodun içine taşır.
+//ama bu build süresinde hız kaybına yol açar iyice değerlendirmek gerekir. inline keyword'ünü kullanırken.
+//Çok fazla kullanılıyorsa inline yapmak mantıklı. Build alırken generate edilen dosyayı büyütür.
+
+//Higher order function'ın Parametresinde birden fazla eğer fonksiyonunuz varsa noinline kullanmak isteyebilirsiniz. Bu sayede hiçbirinin nesnesinin üretilmemesini sağlarsınız.
+
