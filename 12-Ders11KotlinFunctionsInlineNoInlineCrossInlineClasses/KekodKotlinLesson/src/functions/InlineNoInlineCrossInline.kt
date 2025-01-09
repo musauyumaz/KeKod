3266e1230bc9059d127c3a82cfd3bdb51431ae8f
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-01-09 23:00:49 +0300

    feat: Added examples for inline, noinline, non-local return, and crossinline usage
    - Added examples for `non-local return` behavior to clarify its impact in lambdas.
    - Explained `crossinline` usage with appropriate use cases to prevent non-local returns.

--- 12-Ders11KotlinFunctionsInlineNoInlineCrossInlineClasses/KekodKotlinLesson/src/functions/InlineNoInlineCrossInline.kt
+++ 12-Ders11KotlinFunctionsInlineNoInlineCrossInlineClasses/KekodKotlinLesson/src/functions/InlineNoInlineCrossInline.kt
@@ -3,6 +3,16 @@ package functions
 
 fun main() {
     runAndPrint({ println(it) }, { println(it) })
+
+    println("Start")
+    val list = listOf(1, 2, 3, 4, 5, 6, 7)
+    list.forEach {
+        if (it == 5) {
+            return
+        }
+        println(it)
+    }
+    println("End")
 }
 
 inline fun runAndPrint(run: (String) -> Unit, noinline logger: (String) -> Unit) {
@@ -15,6 +25,8 @@ fun log(logger: (String) -> Unit): Unit {
     logger("Start")
 }
 
+
+
 //inline keywordü her seferinde fonksiyondan instance oluşmasını engeller. higher order functionın içindeki datayı metodun içine taşır.
 //ama bu build süresinde hız kaybına yol açar iyice değerlendirmek gerekir. inline keyword'ünü kullanırken.
 //Çok fazla kullanılıyorsa inline yapmak mantıklı. Build alırken generate edilen dosyayı büyütür.
@@ -23,7 +35,7 @@ fun log(logger: (String) -> Unit): Unit {
 
 class HomeFragment() {
     val homeListAdapter = HomeListAdapter({
-
+        println("onClick runs")
     })
 }
 
@@ -31,23 +43,31 @@ class HomeListAdapter(onClick: (View) -> Unit) {
     init {
         val homeListViewHolder = HomeListViewHolder()
         homeListViewHolder.bind(onClick)
-        homeListViewHolder bind onClick
+//        homeListViewHolder bind onClick
     }
 }
 
 class HomeListViewHolder() {
-    inline infix fun bind(noinline onClick: (View) -> Unit): Unit {
+    inline infix fun bind(crossinline onClick: (View) -> Unit): Unit {
         val button = Button()
-        button.setOnClickListener(onClick)
-//        button.setOnClickListener {
-//            onClick.invoke(it)
-//        }
+//        button.setOnClickListener(onClick)
+        button.setOnClickListener {
+            onClick.invoke(it)
+        }
     }
 }
 
 class Button() {
     fun setOnClickListener(onClick: (View) -> Unit): Unit {
+        println("setOnClick runs")
+        val view = View()
+        onClick.invoke(view)
+    }
 
+    init {
+        setOnClickListener {
+            println("init: setOnClick runs")
+        }
     }
 }
 
