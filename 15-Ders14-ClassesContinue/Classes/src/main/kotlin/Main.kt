29b18267f9c80be7d4755a290eb132f0c7b3c58c
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-02 14:11:37 +0300

    feat: Added examples for secondary constructor

--- 15-Ders14-ClassesContinue/Classes/src/main/kotlin/Main.kt
+++ 15-Ders14-ClassesContinue/Classes/src/main/kotlin/Main.kt
@@ -5,6 +5,8 @@ fun main() {
     Customer("Musa")
     Person3("Musa", "Uyumaz")
     Person3("Musa", "Uyumaz", false)
+    Constructors(4)
+//    DontCreateMe()
 }
 
 class Person(firstName: String) {}
@@ -31,6 +33,34 @@ class Customer(name: String) {
 
 class Person2(val firstName: String, val lastName: String, var age: Int)
 
-class Person3(val firstName: String, val lastName: String, var isEmployed: Boolean = true, )
+class Person3(val firstName: String, val lastName: String, var isEmployed: Boolean = true)
 
 //class Customer2 public @Inject constructor(name:String){}
+
+class Person4(val pets: MutableList<Pet> = mutableListOf())
+
+class Pet {
+    constructor(owner: Person4) {
+        owner.pets.add(this)
+    }
+}
+
+class Person5(val name: String) {
+    val children: MutableList<Person5> = mutableListOf()
+
+    constructor(name: String, parent: Person5) : this(name) {
+        parent.children.add(this)
+    }
+}
+
+class Constructors {
+    init {
+        println("Init Block")
+    }
+
+    constructor(i: Int) {
+        println("Constructor $i")
+    }
+}
+
+class DontCreateMe private constructor() {}
