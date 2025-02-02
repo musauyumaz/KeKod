782bd75b691a08f8416baf38ac15d4d4ab1f0fe2
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-02 21:36:35 +0300

    feat: Added examples for override, subclass instantiation, and super usage

--- 15-Ders14-ClassesContinue/Classes/src/main/kotlin/Inheritance.kt
+++ 15-Ders14-ClassesContinue/Classes/src/main/kotlin/Inheritance.kt
@@ -1,7 +1,8 @@
 package org.example
 
 fun main() {
-
+    val derived = Derived3("Hello", "World")
+    val base = Base3("Hello")
 }
 
 class Example : Any() {
@@ -18,3 +19,88 @@ final class Base
 open class Base2(p: Int)
 
 class Derived(p: Int) : Base2(p)
+
+open class Shape {
+    open fun draw() {}
+    final fun fill() {}
+}
+
+class Circle() : Shape() {
+    override fun draw() {}
+}
+
+open class Rectangle2() : Shape() {
+    final override fun draw() {}
+}
+
+class ChildRectangle() : Rectangle2() {
+    fun adssadad(): Unit {
+        draw()
+    }
+}
+
+open class Shape2 {
+    open val vertexCount = 0
+}
+
+class Rectangle3 : Shape2() {
+    override val vertexCount = 4
+}
+
+interface Shape3 {
+    val vertexCount: Int
+}
+
+class Rectangle4(override val vertexCount: Int) : Shape3
+
+class Polygon3 : Shape3 {
+    override var vertexCount: Int = 0
+}
+
+open class Base3(val name: String) {
+    init {
+        println("Initializing base class")
+    }
+
+    open val size: Int = name.length.also(::println)
+}
+
+class Derived3(name: String, val lastName: String) :
+    Base3(name.replaceFirstChar { it.uppercase() }.also { println() }) {
+    init {
+        println("Initializing a derived clas")
+    }
+
+    override val size: Int = (super.size + lastName.length).also { println("") }
+}
+
+open class Rectangle5 {
+    open fun draw() {
+        println("Drawing a rectangle")
+    }
+
+    val borderColor: String get() = "black"
+}
+
+class FilledRectangle5 : Rectangle5() {
+    override fun draw() {
+        super.draw()
+        println("Filling the rectangle")
+    }
+
+    val fillColor: String get() = super.borderColor
+}
+
+open class Rectangle6 {
+    open fun draw() {}
+}
+
+interface Polygon6 {
+    fun draw() {}
+}
+class Square6() : Rectangle6(), Polygon6{
+    override fun draw() {
+        super<Rectangle6>.draw()
+        super<Polygon6>.draw()
+    }
+}
