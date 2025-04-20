26cc20d88c793f6b943c0e87046d9354f1f40a2f
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-20 15:34:51 +0300

    feat: Studied Kotlin reflection for runtime class and property inspection

---
+++ 21-Ders20-ObjectsReflection/Reflection/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation(kotlin("reflect"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
