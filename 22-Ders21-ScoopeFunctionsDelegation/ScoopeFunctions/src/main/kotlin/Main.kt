f9419be694c8863edf27f6b9b7ea75d65831bc86
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-22 13:13:31 +0300

    feat: Explored Kotlin 'run' function for scoped execution and return values

--- 22-Ders21-ScoopeFunctionsDelegation/ScoopeFunctions/src/main/kotlin/Main.kt
+++ 22-Ders21-ScoopeFunctionsDelegation/ScoopeFunctions/src/main/kotlin/Main.kt
@@ -1,22 +1,34 @@
 package org.example
 
 fun main() {
-    val name: String? = "null"
+    val name: String? = "Musa"
 
-    // T = Önüne gelen değişken, R ise son satır
-    val returnValue = name?.let { name: String ->
+    // T = Önüne gelen değişken
+    // R = son satır
+    //Lambda param = T
+    val returnValueLet = name?.let { name: String ->
         println("let $name")
     }
 
-    println(returnValue)
+    println(returnValueLet)
 
-    name?.run {
-        println("run variable")
+    // T = Önüne gelen değişken
+    // R = son satır
+    //Lambda param = yok, this = T
+    val returnValueRun = name?.run {
+        println("run $this")
     }
 
-    run {
+    println(returnValueRun)
+
+    // T = yok, extension işlemi yok
+    // R = son satır
+    //Lambda param = yok, this = yok, çünkü extension yok
+    val returnValueRunJustRun = run {
         println("just run")
     }
+
+    println(returnValueRunJustRun)
 
     with(name) {
         println("with")
@@ -28,4 +40,16 @@ fun main() {
     name?.also {
         println("also")
     }
+}
+
+inline fun <T, R> T.let(block: (T) -> R): R {
+    return block(this)
+}
+
+inline fun <T, R> T.run(block: T.() -> R): R {
+    return block()
+}
+
+inline fun <R> run(block: () -> R): R {
+    return block()
 }
